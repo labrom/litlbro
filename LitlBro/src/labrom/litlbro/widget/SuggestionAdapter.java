600f175fdaf1d2a518246ac1eb57d6e818466792
Romain Laboisse <labrom@gmail.com>
2012-10-01 22:08:23 -0700

    Fix bug caused by notifications occurring on background thread
    09-18 09:34:54.840 E/AndroidRuntime(11012): FATAL EXCEPTION: AsyncTask
    #2
    09-18 09:34:54.840 E/AndroidRuntime(11012): java.lang.RuntimeException:
    An error occured while executing doInBackground()
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.os.AsyncTask$3.done(AsyncTask.java:278)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    java.util.concurrent.FutureTask$Sync.innerSetException(FutureTask.java:2
    73)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    java.util.concurrent.FutureTask.setException(FutureTask.java:124)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    java.util.concurrent.FutureTask$Sync.innerRun(FutureTask.java:307)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    java.util.concurrent.FutureTask.run(FutureTask.java:137)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.jav
    a:1076)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.ja
    va:569)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    java.lang.Thread.run(Thread.java:856)
    09-18 09:34:54.840 E/AndroidRuntime(11012): Caused by:
    android.view.ViewRootImpl$CalledFromWrongThreadException: Only the
    original thread that created a view hierarchy can touch its views.
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.view.ViewRootImpl.checkThread(ViewRootImpl.java:4039)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.view.ViewRootImpl.requestLayout(ViewRootImpl.java:709)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.view.View.requestLayout(View.java:12675)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.view.View.requestLayout(View.java:12675)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.view.View.requestLayout(View.java:12675)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.view.View.requestLayout(View.java:12675)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.view.View.requestLayout(View.java:12675)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.widget.AbsListView.requestLayout(AbsListView.java:1715)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.widget.AdapterView$AdapterDataSetObserver.onChanged(AdapterView.
    java:801)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.widget.AbsListView$AdapterDataSetObserver.onChanged(AbsListView.
    java:5627)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.database.DataSetObservable.notifyChanged(DataSetObservable.java:
    35)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.widget.BaseAdapter.notifyDataSetChanged(BaseAdapter.java:50)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    labrom.litlbro.widget.SuggestionAdapter.update(SuggestionAdapter.java:91
    )
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    java.util.Observable.notifyObservers(Observable.java:138)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    java.util.Observable.notifyObservers(Observable.java:113)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    labrom.litlbro.util.ObservableListProxy.notifyObservers(ObservableListPr
    oxy.java:37)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    labrom.litlbro.util.ObservableListProxy.forceNotify(ObservableListProxy.
    java:31)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    labrom.litlbro.suggestion.SuggestionList.merge(SuggestionList.java:77)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    labrom.litlbro.suggestion.GetSuggestionsTask.doInBackground(GetSuggestio
    nsTask.java:55)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    labrom.litlbro.suggestion.GetSuggestionsTask.doInBackground(GetSuggestio
    nsTask.java:1)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    android.os.AsyncTask$2.call(AsyncTask.java:264)
    09-18 09:34:54.840 E/AndroidRuntime(11012): at
    java.util.concurrent.FutureTask$Sync.innerRun(FutureTask.java:305)
    09-18 09:34:54.840 E/AndroidRuntime(11012): ... 4 more

--- LitlBro/src/labrom/litlbro/widget/SuggestionAdapter.java
+++ LitlBro/src/labrom/litlbro/widget/SuggestionAdapter.java
@@ -11,6 +11,8 @@ import labrom.litlbro.suggestion.Suggestion;
 import labrom.litlbro.util.ObservableListProxy;
 import labrom.litlbro.util.UrlUtil;
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,10 +26,18 @@ public class SuggestionAdapter extends BaseAdapter implements Observer {
     private ObservableListProxy<Suggestion> suggestions;
     private LayoutInflater inflater;
     
-    
+    private final Handler handler;
+    private final Runnable notifier = new Runnable() {
+		
+		@Override
+		public void run() {
+			notifyDataSetChanged();
+		}
+	};
     
     public SuggestionAdapter(Context ctx, List<Suggestion> suggs) {
         this.inflater = (LayoutInflater)ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.handler = new Handler(Looper.getMainLooper());
         setSuggestions(suggs);
     }
     
@@ -51,44 +61,7 @@ public class SuggestionAdapter extends BaseAdapter implements Observer {
     @Override
     public void update(Observable o, Object data) {
         Log.v(L.TAG, "Content changed, notifying");
-        // FIXME
-        /*
-        03-20 08:52:12.390: V/LITLBRO(1844): Content changed, notifying
-        03-20 08:52:12.390: W/dalvikvm(1844): threadid=19: thread exiting with uncaught exception (group=0x4001e578)
-        03-20 08:52:12.400: E/AndroidRuntime(1844): FATAL EXCEPTION: AsyncTask #5
-        03-20 08:52:12.400: E/AndroidRuntime(1844): java.lang.RuntimeException: An error occured while executing doInBackground()
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.os.AsyncTask$3.done(AsyncTask.java:200)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at java.util.concurrent.FutureTask$Sync.innerSetException(FutureTask.java:274)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at java.util.concurrent.FutureTask.setException(FutureTask.java:125)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at java.util.concurrent.FutureTask$Sync.innerRun(FutureTask.java:308)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at java.util.concurrent.FutureTask.run(FutureTask.java:138)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1088)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:581)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at java.lang.Thread.run(Thread.java:1019)
-        03-20 08:52:12.400: E/AndroidRuntime(1844): Caused by: android.view.ViewRoot$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.view.ViewRoot.checkThread(ViewRoot.java:3088)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.view.ViewRoot.requestLayout(ViewRoot.java:669)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.view.View.requestLayout(View.java:8406)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.view.View.requestLayout(View.java:8406)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.view.View.requestLayout(View.java:8406)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.view.View.requestLayout(View.java:8406)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.widget.AbsListView.requestLayout(AbsListView.java:1202)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.widget.AdapterView$AdapterDataSetObserver.onChanged(AdapterView.java:790)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.database.DataSetObservable.notifyChanged(DataSetObservable.java:31)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.widget.BaseAdapter.notifyDataSetChanged(BaseAdapter.java:50)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at labrom.litlbro.suggestion.SuggestionAdapter.update(SuggestionAdapter.java:52)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at java.util.Observable.notifyObservers(Observable.java:139)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at java.util.Observable.notifyObservers(Observable.java:114)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at labrom.litlbro.util.ObservableListProxy.notifyObservers(ObservableListProxy.java:37)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at labrom.litlbro.util.ObservableListProxy.forceNotify(ObservableListProxy.java:31)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at labrom.litlbro.suggestion.SuggestionList.merge(SuggestionList.java:77)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at labrom.litlbro.suggestion.GetSuggestionsTask.doInBackground(GetSuggestionsTask.java:59)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at labrom.litlbro.suggestion.GetSuggestionsTask.doInBackground(GetSuggestionsTask.java:1)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at android.os.AsyncTask$2.call(AsyncTask.java:185)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     at java.util.concurrent.FutureTask$Sync.innerRun(FutureTask.java:306)
-        03-20 08:52:12.400: E/AndroidRuntime(1844):     ... 4 more         
-        */
-        notifyDataSetChanged();
+        this.handler.post(this.notifier);
     }
 
 
